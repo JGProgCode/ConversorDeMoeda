d13d8b6fd235ad1c874480f2044cc474a1ab69b2
JGProgCode <joaogperobeli@gmail.com>
2025-01-20 18:00:23 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import main.com.UI.UIOp;
+import main.com.conversormoedas.api.CurrencyConverterService;
+import com.google.gson.Gson;
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.setProperty("file.encoding", "UTF-8");
+        UIOp.inicio();
+
+
+        }
+    }
